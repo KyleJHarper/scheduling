e40a0952f0039c2733251177fdea7e09959c6a3a
Kyle Harper <KyleJHarper@gmail.com>
2016-12-04 21:55:23 -0800

    Got most of the validation finished.  Need to calculate times and determine the critical path.

--- make_diagram.rb
+++ make_diagram.rb
@@ -12,18 +12,31 @@
   "jobH" => {"duration" => 45,   "dependencies" => ["jobG"],         "description" => "This becomes a seconardy end point"},
   "jobI" => {"duration" => 20,   "dependencies" => ["jobF", "jobH"], "description" => "This becomes another endpoint"},
 
+  # Debugging jobs. They trigger failures (on purpose).
+  # "jobP" => {"duration" => 15,   "dependencies" => [],               "description" => ""},   # 1. Duplicate job name
+  # "jobQ" => {"duration" => 15,   "dependencies" => ["jobQ"],         "description" => ""},   # 2. Dependency of self
+  # "jobR" => {"duration" => 15,   "dependencies" => ["nope"],         "description" => ""},   # 5. Dependencies must exist in schedule.
+  # "jobS" => {                    "dependencies" => [],               "description" => ""},   # 6. Duration key must exist.
+  # "jobT" => {"duration" => 0,    "dependencies" => [],               "description" => ""},   # 6. Duration must be greater than 0.
+
   "jobX" => {"duration" => 15,   "dependencies" => [],               "description" => "The real initial job to ensure hash order doesn't matter"},
 }
 # Validate the schedule...
-# -- Kill duplicates because users make me cry.
+# 1. Kill duplicates because users make me cry.
 @duplicate_jobs = @schedule.keys.detect {|k| @schedule.keys.count(k) > 1}
 abort("You cannot have duplicate jobs in a schedule: #{@duplicate_jobs}") if @duplicate_jobs
+# 2. Job cannot contain a dependency of itself.
 @schedule.map {|job, attributes| abort("You cannot make a job a dependency of itself...: #{job}") if attributes["dependencies"].include?(job)}
-# -- While not fatal, squash any duplicate dependencies to avoid calling .uniq a million times.
+# 3. While not fatal, squash any duplicate dependencies to avoid calling .uniq a million times.
 @schedule.map {|job, attributes| attributes["dependencies"].uniq! }
-# -- TODO There must be at least 1 job that has no dependencies... otherwise it's cyclical and/or incomplete.
-# -- TODO All dependencies for all jobs must exist as pending jobs in the schedule... duh.
-
+# 4. There must be at least 1 job that has no dependencies... otherwise it's cyclical and/or incomplete.
+abort("You must specify at least one root job (a job with no dependencies), otherwise the schedule is cyclical and/or incomplete.") if @schedule.select{|job, attributes| attributes["dependencies"].empty? }.empty?
+# 5. All dependencies for all jobs must exist as pending jobs in the schedule... duh.  (If a dep exists for a job from another schedule, then those schedules are actually integral.)
+@deps_not_in_schedule = @schedule.select{|job,attributes| !(attributes["dependencies"] - @schedule.keys).empty? }.keys
+abort("The following jobs have dependencies that don't exist in the schedule, this shouldn't happen: #{@deps_not_in_schedule}") if !@deps_not_in_schedule.empty?
+# 6. All jobs must have a duration and it must be greater than zero.
+@invalid_durations = @schedule.select{|job,attributes| !attributes.has_key?("duration") || attributes["duration"] < 1 }.keys
+abort("The following jobs are either missing a duration or have a duration of zero: #{@invalid_durations}") if !@invalid_durations.empty?
 
 # Try to figure out where the paths lead so we can set top and left properties correctly.
 @completed = []
@@ -53,6 +66,6 @@ while ! (@schedule.keys - @completed).empty?
     while tops_used.include?(lowest_top) ; lowest_top = lowest_top + TOP_SPACING ; end
     @schedule[job]["top"] = lowest_top
     tops_used.push(lowest_top)
-    puts "Job #{job} appears to have these predicates: #{predicates} with a lowest_top of: #{lowest_top}"
+    puts "Job #{job} appears to have these predicates: #{predicates} with a lowest_top of: #{lowest_top}  and a left of: #{left}"
   }
 end
