99e174e8b142390bab7f5e853a6a37cbecd3c79a
Kyle Harper <KyleJHarper@gmail.com>
2016-12-09 09:46:54 -0800

    Got the html output working.  I even got the color coding to find the critical paths.  Unfortunately, I found a minor bug:  if the schedule has more than 1 final endpoint (which I believe is a valid state to be in) it will calculate slack and stuff wrong.  I need to manually re-run the numbers to confirm it.

--- make_diagram.rb
+++ make_diagram.rb
@@ -1,19 +1,45 @@
 #!/usr/bin/ruby
 
+#
+# Function Definitions
+#
+def printActivity(job, attributes)
+  # Print out a div that will display a pert-style activity.
+  crit = "";
+  crit = " act_critcal" if attributes["slack"] == 0
+  puts "      <div id='#{job}' class='item' style='left: #{attributes['left'] * 200}px; top: #{attributes['top'] * 200}px;'>"
+  puts "        <div class='act_banner'>#{job}</div>"
+  puts "        <div class='act_row'>"
+  puts "          <div class='activity act_narrow'>#{attributes['early_start']}</div>"
+  puts "          <div class='activity act_wide act_id'>&nbsp;</div>"
+  puts "          <div class='activity act_narrow'>#{attributes['late_start']}</div>"
+  puts "        </div>"
+  puts "        <div class='act_row'>"
+  puts "          <div class='activity act_narrow #{crit}'>#{attributes['slack']}</div>"
+  puts "          <div class='activity act_desc'>#{attributes['description']}</div>"
+  puts "        </div>"
+  puts "        <div class='act_row'>"
+  puts "          <div class='activity act_narrow'>#{attributes['early_finish']}</div>"
+  puts "          <div class='activity act_wide act_id'>#{attributes['duration']}</div>"
+  puts "          <div class='activity act_narrow'>#{attributes['late_finish']}</div>"
+  puts "        </div>"
+  puts "      </div>"
+end
+
+
 # Load YAML and the data.
 require 'yaml'
 @data = YAML.load_file('data.yaml')
 @schedule = @data["schedule"]
-puts @schedule
 
 # Validate the schedule...
 # 1. Kill duplicates because users make me cry.
 @duplicate_jobs = @schedule.keys.detect {|k| @schedule.keys.count(k) > 1}
 abort("You cannot have duplicate jobs in a schedule: #{@duplicate_jobs}") if @duplicate_jobs
 # 2. Job cannot contain a dependency of itself.
-@schedule.map {|job, attributes| abort("You cannot make a job a dependency of itself...: #{job}") if attributes["dependencies"].include?(job)}
+@schedule.each {|job, attributes| abort("You cannot make a job a dependency of itself...: #{job}") if attributes["dependencies"].include?(job)}
 # 3. While not fatal, squash any duplicate dependencies to avoid calling .uniq a million times.
-@schedule.map {|job, attributes| attributes["dependencies"].uniq! }
+@schedule.each {|job, attributes| attributes["dependencies"].uniq! }
 # 4. There must be at least 1 job that has no dependencies... otherwise it's cyclical and/or incomplete.
 abort("You must specify at least one root job (a job with no dependencies), otherwise the schedule is cyclical and/or incomplete.") if @schedule.select{|job, attributes| attributes["dependencies"].empty? }.empty?
 # 5. All dependencies for all jobs must exist as pending jobs in the schedule... duh.  (If a dep exists for a job from another schedule, then those schedules are actually integral.)
@@ -40,7 +66,7 @@ while ! (@schedule.keys - @completed).empty?
   left = left + 1
   # Calculate the appropriate 'top' value for vertical positioning later, for each job.
   tops_used = []
-  @current.map {|job, attributes|
+  @current.each {|job, attributes|
     # -- Get the 'top' value(s) of the dependencies.  Attempt to use the lowest of the parents; 0 if none.
     lowest_top = 0
     attributes["dependencies"].each {|dependency| lowest_top = @schedule[dependency]["top"]  if @schedule[dependency].has_key?("top")}
@@ -68,7 +94,7 @@ while ! @unfinished.keys.empty?
   @current = @unfinished.select{|job,attributes| @unfinished.select{|s_job,s_attributes| s_attributes["dependencies"].include?(job) }.keys.empty? }
   abort("Didn't find any jobs to work with to calculate late start and finish.  This shouldn't happen.") if @current.keys.empty?
   # Calculate the actual values for each currently available task, then remove it from the unfinished hash.
-  @current.map {|job, attributes|
+  @current.each {|job, attributes|
     if attributes.has_key?("late_finish")
       @unfinished.delete(job)
       next
@@ -84,16 +110,36 @@ while ! @unfinished.keys.empty?
   }
 end
 
-puts ""
-@schedule.map {|job, attributes|
-  puts "Job #{job}"
-  puts "  dependencies: #{@schedule[job]['dependencies']}"
-  #puts "  Top/Left    : #{@schedule[job]['top']}/#{@schedule[job]['left']}"
-  puts "  ES/Dur/EF   : #{@schedule[job]['early_start']}       #{@schedule[job]['early_finish']}"
-  puts "  Slack       : #{@schedule[job]['slack']}"
-  puts "  EF/Dur/LF   : #{@schedule[job]['late_start']}  #{@schedule[job]['duration']}  #{@schedule[job]['late_finish']}"
-}
 
 # Now build the actual HTML file.
-LEFT_SPACING=200
-TOP_SPACING=200
+# -- Build the HTML, HEAD, and links to Scripts/CSS
+puts "<html>"
+puts "  <head>"
+puts '    <script type="text/javascript" src="jsPlumb-2.2.6.js"></script>'
+puts '    <script type="text/javascript" src="diagram.js"></script>'
+puts '    <link rel="stylesheet" type="text/css" href="diagram.css" media="screen" />'
+puts "  </head>"
+# -- Start the body and insert the divs.
+puts "  <body>"
+puts '    <div id="diagramContainer">'
+@schedule.each {|job,attributes| printActivity(job, attributes) }
+puts "    </div>"
+# -- Load a script to initialize jsPlumb now, then run the snapto's.
+puts '    <script>'
+puts '      jsPlumb.ready(function() {'
+puts '        jsPlumb.setContainer("diagramContainer");'
+puts '        jsPlumb.Defaults.Connector = [ "Flowchart", { stub: [10, 10], midpoint: 0.0001, cornerRadius: 1.5 } ];'
+@schedule.each {|job,attributes|
+  attributes["dependencies"].each {|dependency|
+    color = 'lightblue'
+    color = 'red' if attributes["slack"] == 0 && @schedule[dependency]["slack"] == 0
+    puts "        snapto('#{dependency}', '#{job}', '#{color}')"
+  }
+}
+puts '      });'
+puts '    </script>'
+
+
+# -- Close out the body and html page.
+puts "  </body>"
+puts "</html>"
